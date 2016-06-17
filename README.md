4f4479fcf53851004b142d5a4a692b5adbb9c57b
Kuba Florczuk <kuba.florczuk@arsthanea.com>
2016-06-17 15:13:51 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mac-setup
-setup new mac using ansible
+# Automatic new Mac setup
+
+One-liner to rule them all ! ! !
+
+```bash
+$ bash < (curl -s https://raw.githubusercontent.com/genjusz/mac-setup/master/bootstrap.sh)
+```
---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+ANSIBLE_CONFIGURATION_DIRECTORY="$HOME/.ansible_mac_setup"
+
+# Download and install Command Line Tools with a checking heuristic
+if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]]; then
+    echo "Info   | Install   | xcode"
+    xcode-select --install
+fi
+
+# Download and install Homebrew
+if [[ ! -x /usr/local/bin/brew ]]; then
+    echo "Info   | Install   | homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Info   | Update   | brew"
+	brew update
+fi
+
+# Modify the PATH
+export PATH=/usr/local/bin:$PATH
+
+# Download and install git
+if [[ ! -x /usr/local/bin/git ]]; then
+    echo "Info   | Install   | git"
+    brew install git
+fi
+
+# Download and install Ansible
+if [[ ! -x /usr/local/bin/ansible ]]; then
+    brew install ansible
+fi
+
+# Clone down the Ansible repo
+if [[ ! -d $ANSIBLE_CONFIGURATION_DIRECTORY ]]; then
+    git clone https://github.com/genjusz/mac-setup $ANSIBLE_CONFIGURATION_DIRECTORY
+fi
+
+cd $ANSIBLE_CONFIGURATION_DIRECTORY
+git pull
+ansible-playbook main.yml -i inventory -u $(whoami) -U $(whoami) --ask-sudo-pass
